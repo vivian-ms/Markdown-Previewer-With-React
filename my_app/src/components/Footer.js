1a62e9a2e9c16c12e3d8bd09b0375c80b434e156
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2020-06-14 22:46:00 -0700

    Created Footer component

---
+++ my_app/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="p-2 small">
+      <a href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer" target="_blank" rel="noopener noreferrer">
+        Markdown Previewer Project
+      </a> created for freeCodeCamp
+    </footer>
+  );
+};
+
+export default Footer;
